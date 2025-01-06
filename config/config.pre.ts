f6fe41d048d9ffd2ee1297e95b51b931d3da854d
zjwshisb <361340272@qq.com>
2025-01-06 16:38:23 +0800

    fix(config): fixed loading.js not found

--- config/config.pre.ts
+++ config/config.pre.ts
@@ -1,11 +1,23 @@
 // https://umijs.org/config/
 import { defineConfig } from '@umijs/max';
-
+import { join } from 'path';
+const PUBLIC_PATH = '/server/';
 export default defineConfig({
   define: {
     BASE_URL: 'http://120.77.242.145:8080/api/backend',
     WS_URL: 'http://120.77.242.145:8080/api/backend/ws',
   },
-  publicPath: '/server/',
-  base: '/server/',
+  publicPath: PUBLIC_PATH,
+  base: PUBLIC_PATH,
+  /**
+   * @name <head> 中额外的 script
+   * @description 配置 <head> 中额外的 script
+   */
+  headScripts: [
+    // 解决首次加载时白屏的问题
+    {
+      src: join(PUBLIC_PATH, 'scripts/loading.js'),
+      async: true,
+    },
+  ],
 });
